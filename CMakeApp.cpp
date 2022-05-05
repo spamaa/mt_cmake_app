0b05c300e0cb2cff8c1a9d18073f4f2c132b2457
Grzegorz Dymek <g_dymek@gjpdata.com>
2022-05-05 15:27:25 +0200

    1st version with linking static library (debug/release)

---
+++ CMakeApp.cpp
@@ -0,0 +1,16 @@
+﻿// CMakeApp.cpp : Defines the entry point for the application.
+//
+
+#include "CMakeApp.h"
+#include <iostream>
+#include <beicode.h>
+
+using namespace std;
+
+int main()
+{
+	cout << "Message from beicode" << endl;
+	beicode::About();
+
+	return 0;
+}
---
+++ CMakeApp.h
@@ -0,0 +1,4 @@
+﻿// CMakeApp.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
