bb9ea68b773c64d2f3f3dfdbb02fab62f29812cd
Grzegorz Dymek <g_dymek@gjpdata.com>
2022-05-10 09:31:58 +0200

    in progress

--- CMakeApp.cpp
+++ CMakeApp.cpp
@@ -4,18 +4,33 @@
 #include "CMakeApp.h"
 #include <iostream>
 #include <CMakeTopLib.h>
+#include <fmt/format.h>
 
 using namespace std;
 
 int main()
 {
+	// call method from top lib
 	::std::cout << "===" << ::std::endl;
 	::CMakeTopLib::About();
 	::std::cout << ::std::endl;
 
+	// call method from top lib which calls method from underlaying lib
 	::std::cout << "===" << ::std::endl;
 	CMakeTopLib::AboutStaticLib();
 	::std::cout << ::std::endl;
+
+	// call method from library from default registry
+	::fmt::print("===\n");
+	::fmt::print("This is print from fmt: '{}:{}'\n", __FILE__, __LINE__);
+	::fmt::print("\n");
+
+	// call method from top library that triggers ASAN error
+	::CMakeTopLib::MakeMemError();
+
+	// ASAN error
+	int x[3] = {0, 1, 2};
+	::std::cout << "x[3]: '" << x[3] << ::std::endl;
 
 	return 0;
 }
--- cmake_toolchains/msvc.cmake
+++
@@ -1,8 +0,0 @@
-# set msvc compiler
-set (CMAKE_C_COMPILER cl.exe)
-set (CMAKE_CXX_COMPILER cl.exe)
-
-# configure vcpkg
-include ("c:/work/repos/public/vcpkg/scripts/buildsystems/vcpkg.cmake")
-set (VCPKG_OVERLAY_TRIPLETS c:/work/repos/matterfi/VcpkgPortProject/triplets)
-set (VCPKG_FEATURE_FLAGS "registries,manifest")
